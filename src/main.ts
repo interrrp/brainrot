64623d461bd2337f089310a2f4d4f2d630328078
interrrp <int@addictedto.coffee>
2023-11-26 20:55:37 +0800

    Create Discord bot

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,9 @@
+import { createBot, GatewayIntents, startBot } from "discordeno";
 import "./config.ts";
 
-console.log("Hello, world!");
+const bot = createBot({
+  token: Deno.env.get("DISCORD_TOKEN")!,
+  intents: GatewayIntents.Guilds | GatewayIntents.GuildMessages,
+});
+
+await startBot(bot);
